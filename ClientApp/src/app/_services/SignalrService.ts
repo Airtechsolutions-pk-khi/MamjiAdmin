49f678b0a3b6dda733e52489b62cb3f1911d224a
Moiz Ahmed Khan <moiz75.khan@gmail.com>
2023-04-26 18:24:36 +0500

    .

--- ClientApp/src/app/_services/SignalrService.ts
+++ ClientApp/src/app/_services/SignalrService.ts
@@ -12,26 +12,12 @@ export class SignalrService{
   constructor(public ts: ToastService) {
   }
 
-  public startConnection() {
+  public async startConnection() {
     debugger;
     this.hubConnection = new signalR.HubConnectionBuilder()
       .withUrl('http://localhost:59660/Notify').build();
 
-    this.hubConnection.start().then(() => {
-      console.log('connection started');
-    }).catch(err => console.log(err));
-
-    this.hubConnection.onclose(() => {
-      debugger;
-      setTimeout(() => {
-        console.log('try to re start connection');
-        debugger;
-        this.hubConnection.start().then(() => {
-          debugger;
-          console.log('connection re started');
-        }).catch(err => console.log(err));
-      }, 5000);
-    });
+    await this.hubConnection.start();
 
     this.hubConnection.on('privateMessageMethodName', (data) => {
       debugger;
@@ -40,10 +26,9 @@ export class SignalrService{
 
     this.hubConnection.on('publicMessageMethodName', (data: Notification) => {
       debugger;
-      //console.log('public Message:' + data);
-      // this.showNotification(data);
+      // console.log('public Message:' + data.message);
       this.ts.showWarning(data.title, data.message);
-      //alert(data.message);
+      // alert(data.message);
     });
 
     this.hubConnection.on('clientMethodName', (data) => {
@@ -57,33 +42,4 @@ export class SignalrService{
       this.hubConnection.invoke('GetDataFromClient', 'abc@abc.com', data).catch(err => console.log(err));
     });
   }
-
-  public stopConnection() {
-    this.hubConnection.stop().then(() => {
-      console.log('stopped');
-    }).catch(err => console.log(err));
-  }
-
-  // public startConnection = () => {
-  //     debugger;
-  //     this.hubConnection = new signalR.HubConnectionBuilder()
-  //                             .withUrl('https://localhost:7132/Notify',{ skipNegotiation: true,
-  //                             transport: signalR.HttpTransportType.WebSockets})
-  //                             .build();
-  //     this.hubConnection
-  //     .start()
-  //     .then(() => console.log('Connection started'))
-  //     .catch(err => console.log('Error while starting connection: ' + err))
-  // }
-
-  // public addProductListner = () => {
-  //   this.hubConnection.on('SendMessage', (notification: Notification) => {
-  //     this.showNotification(notification);
-  //   });
-  // }
-
-  // showNotification(notification: Notification) {
-  //   debugger;
-  //   this.ts.warning( notification.message,notification.title);
-  // }
 }
